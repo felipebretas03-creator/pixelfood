31944e7441547775ef74c45b2198059ae3fe822d
Marcos Felipe <marcosfelipe@MacBook-Air-de-Marcos.local>
2026-08-25 14:18:34 -0300

    fix: corrige URL de fetch do dashboard master que estava apontando para rota inexistente

--- painel/src/app/master/page.tsx
+++ painel/src/app/master/page.tsx
@@ -31,7 +31,7 @@ export default function MasterDashboard() {
 
   const fetchDashboard = async () => {
     try {
-      const res = await apiFetch('/api/master/dashboard');
+      const res = await apiFetch('/api/master/restaurants');
       if (res.ok) {
         const json = await res.json();
         setData(json);
